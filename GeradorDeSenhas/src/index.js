9514b1ba5c7916c251bab59b2193060a95d2fe00
arctavares <cosilk@hotmail.com>
2023-09-12 15:55:04 -0300

    importacao funcionando - main.js gerando senha

--- GeradorDeSenhas/src/index.js
+++ GeradorDeSenhas/src/index.js
@@ -1,3 +1,8 @@
 import { nanoid } from "nanoid";
 
-console.log(nanoid())
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+    displayPasswordEl.innerHTML = nanoid()
+})
