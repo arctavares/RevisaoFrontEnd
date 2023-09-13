c83da429f0b9631c691fbd1c6946f94e9f3643ee
arctavares <cosilk@hotmail.com>
2023-09-13 15:52:07 -0300

    page works but some of the ids doesnt work

--- JsAssincrono/SuperHeroAPI/src/main.js
+++ JsAssincrono/SuperHeroAPI/src/main.js
@@ -1,6 +1,9 @@
 const COMPLETE_INFO_URL = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json';
+const INFO_BY_ID = (id) => `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${id}.json`
 
 const sortBtn = document.querySelector('#sortSuperHero');
+const superHeroName = document.querySelector('#SuperHeroName');
+const superHeroImage = document.querySelector('#superHeroImage');
 
 let SuperHerosLength;
 
@@ -12,4 +15,19 @@ const getSuperHeroesLength = async () => {  // get max super heroes number
 
 SuperHerosLength = await getSuperHeroesLength();
 
-console.log(SuperHerosLength);
+const randomInt = () => Math.floor(Math.random() * SuperHerosLength) + 1;
+
+const getSuperHeroInfoById = async (id) => {
+    const response = await fetch(INFO_BY_ID(id))
+    const data = await response.json();
+    return data;
+}
+
+sortBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const randInt = randomInt();
+    const getHero = await getSuperHeroInfoById(randInt)
+    console.log(getHero)
+    superHeroName.innerHTML = getHero.name;
+    superHeroImage.src = getHero.images.md;
+})
