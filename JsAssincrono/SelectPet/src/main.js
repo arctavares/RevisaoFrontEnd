c00718b8e73cda82e257fa39cb258f47de99a1b4
arctavares <cosilk@hotmail.com>
2023-09-13 16:42:56 -0300

    get random dog working, get random cat working

--- JsAssincrono/SelectPet/src/main.js
+++ JsAssincrono/SelectPet/src/main.js
@@ -1,1 +1,34 @@
-console.log('oi')
+const randomDogBtn = document.querySelector('.getRandomDogBtn');
+const randomCatBtn = document.querySelector('.getRandomCatBtn');
+const randomPetBtn = document.querySelector('.getRandomPet');
+
+const petImage = document.querySelector('.petImage');
+
+const DOG_API = 'https://dog.ceo/api/breeds/image/random';
+const CAT_API = 'https://api.thecatapi.com/v1/images/search';
+
+const getRandomDogImage = async () => {
+    const response = await fetch(DOG_API);
+    const data = await response.json();
+    const image_url = data.message;
+    return image_url;
+}
+
+const getRandomCatImage = async () => {
+    const response = await fetch(CAT_API);
+    const data = await response.json();
+    const image_url = data[0].url;
+    return image_url;
+}
+
+randomDogBtn.addEventListener('click',async (e) => {
+    e.preventDefault();
+    const dog_url_image = await getRandomDogImage();
+    petImage.src = dog_url_image;
+})
+
+randomCatBtn.addEventListener('click',async (e) => {
+    e.preventDefault();
+    const cat_url_image = await getRandomCatImage();
+    petImage.src = cat_url_image;
+})
